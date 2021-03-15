cd2512d931c433e20b7790deb6fdb5ff6d6af4f2
Xawe <dreamxawe@hotmail.com>
2021-03-14 21:29:29 -0300

    ajuste em estrutura do projeto

---
+++ HangPlayGround.Model/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HangPlayGround.Model
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        
+    }
+}
---
+++ HangPlayGround.Tests/Service/UserServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace HangPlayGround.Tests.Service
+{
+    public class UserServiceTest
+    {
+
+        private HangPlayGround.Service.IUserServices userServices;
+
+        public UserServiceTest()
+        {
+            userServices = new HangPlayGround.Service.UserService();
+        }
+
+        [Fact]
+        public void LoadUsersTest()
+        {
+            var result = userServices.GetUserList();
+            Assert.True(result.Any());
+        }
+    }
+}
